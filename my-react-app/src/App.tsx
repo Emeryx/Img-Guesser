af4895f4f02b3c7b0a0ed2ecb33f32e5c70a2e01
Emeryx <omergal2003@gmail.com>
2024-06-04 16:51:07 +0300

    Switched front end default font and customized it's typography, Adding a theme extension for the first time

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -5,12 +5,29 @@ import HostGame from './pages/HostGame';
 import GameRoom from './pages/GameRoom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import gameSessionMockup from './assets/gameSessionMockup';
+import { CssVarsProvider, extendTheme } from '@mui/joy';
+
+const theme = extendTheme({
+  fontFamily: {
+    display: 'Noto Sans, var(--joy-fontFamily-fallback)',
+    body: 'Noto Sans, var(--joy-fontFamily-fallback)',
+  },
+  typography: {
+    h1: {
+      fontWeight: 700
+    },
+    h4: {
+      letterSpacing: 0.25
+    }
+  }
+})
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <CssVarsProvider theme={theme}>
       <Router>
           <Routes>
             <Route path='/' Component={Main} />
@@ -18,6 +35,7 @@ function App() {
             <Route path='/r/:roomCode' element={<GameRoom {...gameSessionMockup} />} />
           </Routes>
       </Router>
+      </CssVarsProvider>
     </QueryClientProvider>
   )
 }
