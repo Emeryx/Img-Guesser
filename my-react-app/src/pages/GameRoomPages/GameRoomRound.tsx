27bdcb7f593c61a30b9522e4eac37441e3e5572d
Emeryx <omergal2003@gmail.com>
2024-06-24 13:04:18 +0300

    Uploaded a mock image to the game room round file

--- my-react-app/src/pages/GameRoomPages/GameRoomRound.tsx
+++ my-react-app/src/pages/GameRoomPages/GameRoomRound.tsx
@@ -1,10 +1,16 @@
-import { Stack, Typography } from "@mui/joy";
+import { Box, Stack, Typography } from "@mui/joy";
 import GameRoomPageProps from "./GameRoomPageProps.interface";
-
+import coyote from '../../assets/question_images/Coyote.webp'
+import styles from "../../assets/MuiStyles";
+const {subheaderFontSize} = styles;
 const GameRoomRound: React.FC<GameRoomPageProps> = ({ gameSession, player, display, isGameDataLoading, isPlayerHost }) => {
     return (
-        <Stack>
-            <Typography>-- GAME ROUND --</Typography>
+        <Stack direction='column' justifyContent='center' alignItems='center' spacing={4} sx={{mt: 4}}>
+            {/* <Typography>-- GAME ROUND --</Typography> */}
+            <Typography level='h3' fontSize={subheaderFontSize} textColor='custom.primary'>Guess what this is!</Typography>
+            <Box height={{ xs: 400 ,md: 500, lg: 500 }} width={{ md: '100%', lg: 1000, xl: 1400 }}>
+                <img src={coyote} width='100%' height='100%' style={{objectFit: 'cover'}} />
+            </Box>
         </Stack>
     )
 }
